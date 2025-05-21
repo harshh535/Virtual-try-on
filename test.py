6d06c2eb65b684e53f936258f7573abcfdd25ae6
harshh535 <119890274+harshh535@users.noreply.github.com>
2025-05-21 13:56:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 import argparse
 import os
-
 import torch
 from torch import nn
 from torch.nn import functional as F
 import torchgeometry as tgm
 
+import gdown  # For downloading from Google Drive
+
 from datasets import VITONDataset, VITONDataLoader
 from networks import SegGenerator, GMM, ALIASGenerator
 from utils import gen_noise, load_checkpoint, save_images
+
+
+def download_if_not_exists(file_id, dest_path):
+    if not os.path.exists(dest_path):
+        url = f"https://drive.google.com/uc?id={file_id}"
+        print(f"Downloading {dest_path} from Google Drive...")
+        gdown.download(url, dest_path, quiet=False)
 
 
 def get_opt():
@@ -16,7 +24,7 @@ def get_opt():
     parser.add_argument('--name', type=str, required=True)
 
     parser.add_argument('-b', '--batch_size', type=int, default=1)
-    parser.add_argument('-j', '--workers', type=int, default=1)  # default 1, will override later
+    parser.add_argument('-j', '--workers', type=int, default=1)
     parser.add_argument('--load_height', type=int, default=1024)
     parser.add_argument('--load_width', type=int, default=768)
     parser.add_argument('--shuffle', action='store_true')
@@ -33,27 +41,21 @@ def get_opt():
     parser.add_argument('--gmm_checkpoint', type=str, default='gmm_final.pth')
     parser.add_argument('--alias_checkpoint', type=str, default='alias_final.pth')
 
-    # common
-    parser.add_argument('--semantic_nc', type=int, default=13, help='# of human-parsing map classes')
+    parser.add_argument('--semantic_nc', type=int, default=13)
     parser.add_argument('--init_type', choices=['normal', 'xavier', 'xavier_uniform', 'kaiming', 'orthogonal', 'none'], default='xavier')
-    parser.add_argument('--init_variance', type=float, default=0.02, help='variance of the initialization distribution')
+    parser.add_argument('--init_variance', type=float, default=0.02)
 
-    # for GMM
     parser.add_argument('--grid_size', type=int, default=5)
 
-    # for ALIASGenerator
     parser.add_argument('--norm_G', type=str, default='spectralaliasinstance')
-    parser.add_argument('--ngf', type=int, default=64, help='# of generator filters in the first conv layer')
-    parser.add_argument('--num_upsampling_layers', choices=['normal', 'more', 'most'], default='most',
-                        help='If \'more\', add upsampling layer between the two middle resnet blocks. '
-                             'If \'most\', also add one more (upsampling + resnet) layer at the end of the generator.')
+    parser.add_argument('--ngf', type=int, default=64)
+    parser.add_argument('--num_upsampling_layers', choices=['normal', 'more', 'most'], default='most')
 
     opt = parser.parse_args()
     return opt
 
 
 def test(opt, seg, gmm, alias):
-    # Disable multiprocessing DataLoader workers to avoid shared memory issues
     opt.workers = 0
 
     up = nn.Upsample(size=(opt.load_height, opt.load_width), mode='bilinear')
@@ -73,7 +75,6 @@ def test(opt, seg, gmm, alias):
             c = inputs['cloth']['unpaired']
             cm = inputs['cloth_mask']['unpaired']
 
-            # Part 1. Segmentation generation
             parse_agnostic_down = F.interpolate(parse_agnostic, size=(256, 192), mode='bilinear')
             pose_down = F.interpolate(pose, size=(256, 192), mode='bilinear')
             c_masked_down = F.interpolate(c * cm, size=(256, 192), mode='bilinear')
@@ -88,20 +89,19 @@ def test(opt, seg, gmm, alias):
             parse_old.scatter_(1, parse_pred, 1.0)
 
             labels = {
-                0:  ['background',  [0]],
-                1:  ['paste',       [2, 4, 7, 8, 9, 10, 11]],
-                2:  ['upper',       [3]],
-                3:  ['hair',        [1]],
-                4:  ['left_arm',    [5]],
-                5:  ['right_arm',   [6]],
-                6:  ['noise',       [12]]
+                0: ['background', [0]],
+                1: ['paste', [2, 4, 7, 8, 9, 10, 11]],
+                2: ['upper', [3]],
+                3: ['hair', [1]],
+                4: ['left_arm', [5]],
+                5: ['right_arm', [6]],
+                6: ['noise', [12]]
             }
             parse = torch.zeros(parse_pred.size(0), 7, opt.load_height, opt.load_width, dtype=torch.float)
             for j in range(len(labels)):
                 for label in labels[j][1]:
                     parse[:, j] += parse_old[:, label]
 
-            # Part 2. Clothes Deformation
             agnostic_gmm = F.interpolate(img_agnostic, size=(256, 192), mode='nearest')
             parse_cloth_gmm = F.interpolate(parse[:, 2:3], size=(256, 192), mode='nearest')
             pose_gmm = F.interpolate(pose, size=(256, 192), mode='nearest')
@@ -112,7 +112,6 @@ def test(opt, seg, gmm, alias):
             warped_c = F.grid_sample(c, warped_grid, padding_mode='border')
             warped_cm = F.grid_sample(cm, warped_grid, padding_mode='border')
 
-            # Part 3. Try-on synthesis
             misalign_mask = parse[:, 2:3] - warped_cm
             misalign_mask[misalign_mask < 0.0] = 0.0
             parse_div = torch.cat((parse, misalign_mask), dim=1)
@@ -122,30 +121,46 @@ def test(opt, seg, gmm, alias):
 
             unpaired_names = []
             for img_name, c_name in zip(img_names, c_names):
-                unpaired_names.append('{}_{}'.format(img_name.split('_')[0], c_name))
+                unpaired_names.append(f'{img_name.split("_")[0]}_{c_name}')
 
             save_images(output, unpaired_names, opt.save_dir)
 
             if (i + 1) % opt.display_freq == 0:
-                print("step: {}".format(i + 1))
+                print(f"step: {i + 1}")
 
 
 def main():
     opt = get_opt()
     print(opt)
 
-    if not os.path.exists(opt.save_dir):
-       os.makedirs(opt.save_dir)
+    os.makedirs(opt.save_dir, exist_ok=True)
+    os.makedirs(opt.checkpoint_dir, exist_ok=True)
 
+    # Google Drive file IDs
+    seg_ckpt_id = "1Hb_y7M4pQlrKh6m4-2Mo_m_KU1IcT6DB"
+    gmm_ckpt_id = "1gtagvr1I8Dq4ejnpQ51fZ9G9sCloKgyh"
+    alias_ckpt_id = "1vWoDdaiWF0Zuv8Md9bn7q-xLUUIjXReY"
+
+    # Local paths
+    seg_ckpt_path = os.path.join(opt.checkpoint_dir, opt.seg_checkpoint)
+    gmm_ckpt_path = os.path.join(opt.checkpoint_dir, opt.gmm_checkpoint)
+    alias_ckpt_path = os.path.join(opt.checkpoint_dir, opt.alias_checkpoint)
+
+    # Download from Google Drive if not exists
+    download_if_not_exists(seg_ckpt_id, seg_ckpt_path)
+    download_if_not_exists(gmm_ckpt_id, gmm_ckpt_path)
+    download_if_not_exists(alias_ckpt_id, alias_ckpt_path)
+
+    # Load models
     seg = SegGenerator(opt, input_nc=opt.semantic_nc + 8, output_nc=opt.semantic_nc)
     gmm = GMM(opt, inputA_nc=7, inputB_nc=3)
     opt.semantic_nc = 7
     alias = ALIASGenerator(opt, input_nc=9)
     opt.semantic_nc = 13
 
-    load_checkpoint(seg, os.path.join(opt.checkpoint_dir, opt.seg_checkpoint))
-    load_checkpoint(gmm, os.path.join(opt.checkpoint_dir, opt.gmm_checkpoint))
-    load_checkpoint(alias, os.path.join(opt.checkpoint_dir, opt.alias_checkpoint))
+    load_checkpoint(seg, seg_ckpt_path)
+    load_checkpoint(gmm, gmm_ckpt_path)
+    load_checkpoint(alias, alias_ckpt_path)
 
     seg.eval()
     gmm.eval()
