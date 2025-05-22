c16eb29c64eac7745d96591699d975a56664425d
harshh535 <119890274+harshh535@users.noreply.github.com>
2025-05-22 16:52:22 +0530

    Update automated.py

--- automated.py
+++ automated.py
@@ -146,7 +146,6 @@ def main(cloth_path):
         stderr=subprocess.PIPE,
         text=True
     )
-    st.write("success")
     if proc.stdout:
         print(f"--- test.py STDOUT ---\n{proc.stdout}")
     if proc.stderr:
